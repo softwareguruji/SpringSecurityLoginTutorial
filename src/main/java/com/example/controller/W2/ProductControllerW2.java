4c372e6d13ff185660695ef7e530a5dbdad694d4
ketan Bhavsar <ketan@DESKTOP-NVRKIE8>
2017-08-01 11:34:33 +0530

    Datatable Data update / AJAX call updated on table successfully.
    https://datatables.net/reference/api/ajax.url().load()
    
    Model creation for Add/Update/Delete Alert and Confirmation

--- src/main/java/com/example/controller/W2/ProductControllerW2.java
+++ src/main/java/com/example/controller/W2/ProductControllerW2.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.model.MultiLanguageProduct;
+
 @Controller
 public class ProductControllerW2 {
 
 	@RequestMapping(value = "/admin/w2/listProduct", method = RequestMethod.GET)
 	public ModelAndView listProducts(){
 		ModelAndView modelAndView = new ModelAndView();
+		MultiLanguageProduct multiLangProdObj = new MultiLanguageProduct();
+		modelAndView.addObject("product", multiLangProdObj);
 		modelAndView.setViewName("/admin/ProductListW2/product_list");
 		return modelAndView;
 	}
